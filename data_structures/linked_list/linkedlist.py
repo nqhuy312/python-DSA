bf04941b045045771ea40667ecd748f9edc43f16
Khoa Tran <thdkhoa1312@gmail.com>
2022-05-17 21:44:07 +0700

    dev #5 - Implement delete node

--- data_structures/linked_list/linkedlist.py
+++ data_structures/linked_list/linkedlist.py
@@ -1,4 +1,3 @@
-from re import I
 from node import Node
 
 # =============================================================================
@@ -34,6 +33,7 @@ class LinkedList:
 
         elif not self.__head:
             self.__head = node
+            self.__tail = node
 
         else:
             node.pointer = self.__head
@@ -43,22 +43,46 @@ class LinkedList:
         return self
 
     def add(self, idx, node):
-        if not isinstance(node, Node) or idx > self.__len:
+        if not isinstance(node, Node) or \
+            not 0 <= idx <= self.__len - 1:
             return self
 
         elif not self.__head:
             self.__head = node
+            self.__tail = node
+        elif idx == 0:
+            self.add_front(node)
+        elif idx == self.__len:
+            self.add_rear(node)
         else:
-            run_node = self.__head
-            for i in range(self.__len):
-                curr_node = run_node
-                run_node = curr_node.pointer
-
+            prev_node = self.__head
+            for i in range(1, self.__len):
                 if idx == i:
-                    curr_node.pointer = node
-                    node.pointer = run_node
+                    node.pointer = prev_node.pointer
+                    prev_node.pointer = node
+                    break
+                prev_node = prev_node.pointer
 
         self.__len += 1
+        return self
+
+    def delete(self, idx):
+        if not 0 <= idx <= self.__len - 1:
+            return self
+        elif idx == 0:
+            self.__head = self.__head.pointer
+
+        else:
+            prev_node = self.__head
+            for i in range(1, self.__len):
+                curr_node = prev_node.pointer
+                if idx == i:
+                    prev_node.pointer = curr_node.pointer
+                
+                self.__tail = prev_node
+                prev_node = prev_node.pointer
+
+        self.__len -= 1
         return self
 
     def add_rear(self, node):
@@ -67,6 +91,7 @@ class LinkedList:
 
         elif not self.__head:
             self.__head = node
+            self.__tail = node
         else:
             self.__tail.pointer = node
             self.__tail = node
@@ -86,13 +111,26 @@ class LinkedList:
 # =============================================================================
 
 if __name__ == "__main__":
-    linked_list = LinkedList()
+    linked_list = LinkedList(7,8,9)
 
     four = Node(4)
     zero = Node(0)
     six = Node(6)
+
+    print(linked_list)
+    print(len(linked_list))
     linked_list.add_rear(four)
+    print(linked_list)
+    print(len(linked_list))
     linked_list.add_front(zero)
+    print(linked_list)
+    print(len(linked_list))
     linked_list.add(1, six)
     print(linked_list)
     print(len(linked_list))
+    linked_list.delete(5)
+    print(linked_list)
+    print(len(linked_list))
+    # linked_list.delete_rear()
+    # print(linked_list)
+    # print(len(linked_list))
